fd67ced185edbd48c1d50ad6ed6e3ed9a2e1f286
Sergio Incaser <sergio@incaser.es>
2016-05-04 18:41:52 +0200

    [WIP] website_sale_by_category: Delevelop module

--- website_sale_by_category/__init__.py
+++ website_sale_by_category/__init__.py
@@ -2,5 +2,4 @@
 # © 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import models, wizards, controllers, report
-from .hooks import pre_init_hook, post_init_hook, uninstall_hook
+from . import models
