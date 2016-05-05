bedaa819c93634c1339d0f131ce2f6f31955d00b
sergio-incaser <sergio@incaser.es>
2016-05-05 23:47:26 +0200

    [WIP] website_sale_product_group: Add to cart

--- website_sale_product_group/static/src/js/product_group.js
+++ website_sale_product_group/static/src/js/product_group.js
@@ -10,9 +10,41 @@
         $( ".chk_line" ).change(function () {
             var amount = 0.0;
             $( ".chk_line:checked" ).each(function() {
-                amount += parseFloat($(this).attr('data_oe_lst_price'));
+                amount += parseFloat($(this).data('lst_price'));
             });
             $("#total_selected").text(amount.toFixed(2).toString() +' €');
           }).change();
+        $('#add_to_cart').click(add_to_cart_group);
     });
+
+    function add_to_cart_group(ev){
+        ev.preventDefault();
+        $( ".chk_line:checked" ).each(function() {
+            debugger
+            var $check = $(this);
+            var line_id;
+            var value = 1;
+            openerp.jsonRpc("/shop/cart/update_json", 'call', {
+            'line_id': null,
+            'product_id': parseInt($check.data('product_id')),
+            'set_qty': value})
+            .then(function (data) {
+                debugger
+                if (!data.quantity) {
+                    location.reload(true);
+                    return;
+                }
+                var $q = $(".my_cart_quantity");
+                $q.parent().parent().removeClass("hidden", !data.quantity);
+                $q.html(data.cart_quantity).hide().fadeIn(600);
+
+//                $("#cart_total").replaceWith(data['website_sale.total']);
+            });
+
+
+        });
+
+
+    }
+
 })();
