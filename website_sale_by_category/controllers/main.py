59ca9c409ae941c6c24d9771dc157e226609e99b
Carlos Incaser <carlos@incaser.es>
2016-05-16 19:55:22 +0200

    [WIP] website_sale_by_category: Fix structure

--- website_sale_by_category/controllers/main.py
+++ website_sale_by_category/controllers/main.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 # © 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import http
+from openerp.addons.website_sale.controllers.main import website_sale
 
 
-class MyController(http.Controller):
-    pass
+class WebsiteSale(website_sale):
+
+    def _get_search_domain(self, search, category, attrib_values):
+        domain = super(WebsiteSale, self)._get_search_domain(
+            search, category, attrib_values)
+        if category:
+            orig_condition = ('public_categ_ids', 'child_of', int(category))
+            if orig_condition in domain:
+                pos = domain.index(orig_condition)
+                domain[pos] = ('public_categ_ids', '=', int(category))
+        return domain
