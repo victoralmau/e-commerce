710fc6f5946135a0c1e35af37919612598fa413c
Carlos Incaser <carlos@incaser.es>
2016-05-17 17:49:03 +0200

    [WIP] website_sale_by_category: Category web description

--- website_sale_by_category/controllers/main.py
+++ website_sale_by_category/controllers/main.py
@@ -7,15 +7,4 @@ from openerp.addons.website_sale.controllers.main import website_sale
 
 
 class WebsiteSale(website_sale):
-
-    def _get_search_domain(self, search, category, attrib_values):
-        domain = super(WebsiteSale, self)._get_search_domain(
-            search, category, attrib_values)
-        if category:
-            orig_condition = ('public_categ_ids', 'child_of', int(category))
-            if orig_condition in domain:
-                pos = domain.index(orig_condition)
-                domain[pos] = ('public_categ_ids', '=', int(category))
-        else:
-            domain.append(('id', '=', 0))
-        return domain
+    pass
