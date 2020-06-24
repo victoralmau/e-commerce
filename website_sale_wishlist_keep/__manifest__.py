b15dd7ca48dc5fc511e5e500abc826796a5ce8d8
Carlos Roca <carlos.roca@tecnativa.com>
2020-06-24 13:13:24 +0200

    [ADD] website_sale_wishlist_keep: New module that keep the items on wishlist when we add to cart

---
+++ website_sale_wishlist_keep/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Tecnativa - Carlos Roca
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Wesbite Sale Wishlist Keep",
+    "category": "Website",
+    "summary": 'Allows to add products to my cart but keep it in my wishlist"',
+    "development_status": "Beta",
+    "version": "12.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["website_sale_wishlist"],
+    "demo": ["demo/assets.xml"],
+    "data": ["views/website_sale_wishlist_template.xml"],
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "installable": True,
+}
